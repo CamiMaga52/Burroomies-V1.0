ad2d1f793f68ff837485c2108aa3bd3b67cf0da0
CamiMaga52 <gestion.saeda.contacto@gmail.com>
2026-05-21 14:50:23 -0600

    cambiar puerto para uso de brevo

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 // Configuración SMTP de Brevo
 const transporter = nodemailer.createTransport({
   host: 'smtp-relay.brevo.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.BREVO_SMTP_USER,
     pass: process.env.BREVO_SMTP_PASS
